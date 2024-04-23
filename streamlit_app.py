8f36a3a30813a26d2dca613d454dc446e3f33be8
ibrahim9781 <47012088+ibrahim9781@users.noreply.github.com>
2024-04-23 15:30:16 +0100

    Update streamlit_app.py
    comma was added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.write(""" A login page for a social platform where users can login to there dashboard'''
+st.write(""" A login page for a social platform where users can log in to there dashboard''')
